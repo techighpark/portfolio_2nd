ea826c7fb3c1400d9cb567963052e50255a19a15
Taekwan Park <techigh@Taekwans-MacBook-Pro.local>
2022-07-21 23:14:58 +0900

    7/21

--- components/nav.tsx
+++ components/nav.tsx
@@ -5,27 +5,21 @@ import { ImPacman } from "react-icons/im";
 
 export default function Navigation() {
   return (
-    <div className="border-black flex justify-center">
-      {/* <div className="grid grid-cols-4 gap-3 place-items-center w-1/3 rounded-3xl ">
-        <div className="aspect-square rounded-2xl shadow-xl w-16 hover:shadow-2xl hover:">
-          About
-        </div>
-        <div className="aspect-square rounded-2xl shadow-xl w-16">Skills</div>
-        <div className="aspect-square rounded-2xl shadow-xl w-16">Works</div>
-        <div className="aspect-square rounded-2xl shadow-xl w-16">Contact</div>
-      </div> */}
-      <div className="flex justify-center relative rounded-2xl w-96 h-16 hover:scale-105 transition-all duration-200 backdrop-blur-md bg-gray-300 shadow-inner shadow-white">
-        <div className="aspect-square rounded-2xl shadow-xl w-16 hover:shadow-2xl absolute bottom-5 left-[1rem] hover:cursor-pointer hover:scale-110 transition-all duration-500 text-center flex justify-center items-center bg-slate-100">
-          <SiActigraph />
-        </div>
-        <div className="aspect-square rounded-2xl shadow-xl w-16 hover:shadow-2xl absolute bottom-5 left-[7rem] hover:cursor-pointer hover:scale-110 transition-all duration-500 text-center flex justify-center items-center bg-slate-100">
-          <FaAngellist />
-        </div>
-        <div className="aspect-square rounded-2xl shadow-xl w-16 hover:shadow-2xl absolute bottom-5 left-[13rem] hover:cursor-pointer hover:scale-110 transition-all duration-500 text-center flex justify-center items-center bg-slate-100">
-          <WiAlien />
-        </div>
-        <div className="aspect-square rounded-2xl shadow-xl w-16 hover:shadow-2xl absolute bottom-5 left-[19rem] hover:cursor-pointer hover:scale-110 transition-all duration-500 text-center flex justify-center items-center bg-slate-100">
-          <ImPacman />
+    <div className="flex justify-center">
+      <div className="flex justify-center items-center relative rounded-2xl w-96 h-14 hover:scale-[102%] transition-all duration-200 backdrop-blur-md bg-gray-300 shadow-inner shadow-white ">
+        <div className="w-[23.5rem] h-[3rem] border rounded-xl border-slate-200 shadow-xl">
+          <div className="aspect-square rounded-2xl shadow-xl w-16 hover:shadow-2xl absolute bottom-4 left-[1rem] hover:cursor-pointer hover:scale-110 hover:bg-slate-200 active:bg-slate-300 active:ring-1 active:ring-slate-100 transition-all duration-500 flex justify-center items-center bg-slate-100 hover:bottom-7">
+            <SiActigraph />
+          </div>
+          <div className="aspect-square rounded-2xl shadow-xl w-16 hover:shadow-2xl absolute bottom-4 left-[7rem] hover:cursor-pointer hover:scale-150 transition-all duration-500 flex justify-center items-center bg-slate-100 hover:bottom-7">
+            <FaAngellist />
+          </div>
+          <div className="aspect-square rounded-2xl shadow-xl w-16 hover:shadow-2xl absolute bottom-4 left-[13rem] hover:cursor-pointer hover:scale-110 transition-all duration-500 flex justify-center items-center bg-slate-100 hover:bottom-7">
+            <WiAlien />
+          </div>
+          <div className="aspect-square rounded-2xl shadow-xl w-16 hover:shadow-2xl absolute bottom-4 left-[19rem] hover:cursor-pointer hover:scale-110 transition-all duration-500 flex justify-center items-center bg-slate-100 hover:bottom-7">
+            <ImPacman />
+          </div>
         </div>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,12 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div className="h-96"></div>
+      <div className="h-96">
+        <div className="font-normal font-mono text-7xl">
+          I'm falling in love with 현지 🧡
+        </div>
+        <div className="font-normal font-sans text-3xl">단소 살인마</div>
+      </div>
     </Layout>
   );
 };
